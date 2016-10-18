2196c895075cd7b31f86ff2a6a3baeac36c005ab
sisiea <644022748@qq.com>
2016-10-19 00:17:58 +0800

    1
    1

---
+++ TSLearning/main.js
@@ -0,0 +1,19 @@
+'use strict';
+/**
+ * person
+ */
+class person {
+    constructor() {
+    }
+    /**
+     * print
+     */
+    print() {
+        console.log('this is a person');
+    }
+}
+var p = new person();
+p.print();
+var a = false;
+console.log(a);
+//# sourceMappingURL=main.js.map
---
+++ TSLearning/main.ts
@@ -0,0 +1,23 @@
+'use strict'
+/**
+ * person
+ */
+class person {
+    constructor() {
+        
+    }
+
+    /**
+     * print
+     */
+    public print() {
+        console.log('this is a person')
+    }
+}
+
+var p = new person();
+p.print();
+
+var a:boolean = false;
+
+console.log(a);
